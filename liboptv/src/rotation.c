82b117d310f1087de491f65821a0d6ca0f7ab4c4
Alex Liberzon <alex.liberzon@gmail.com>
2013-06-03 21:53:07 +0300

    there is a change in rotation_matrix after our discussion with Yosef about calibration.c submission

--- liboptv/src/rotation.c
+++ liboptv/src/rotation.c
@@ -21,13 +21,11 @@ Routines contained:		-
 #include <math.h>
 #include "calibration.h"
 
-void rotation_matrix (Ex, dm)
+/* call it with &(Ex[i_img]) */
 
-Exterior  Ex;
-Dmatrix   dm;
+void rotation_matrix (Exterior *Ex) {
 
-{
-    dm[0][0] = cos(Ex.phi) * cos(Ex.kappa);
+    Ex->dm[0][0] = cos(Ex.phi) * cos(Ex.kappa);
     dm[0][1] = (-1) * cos(Ex.phi) * sin(Ex.kappa);
     dm[0][2] = sin(Ex.phi);
     dm[1][0] = cos(Ex.omega) * sin(Ex.kappa)
@@ -42,3 +40,30 @@ Dmatrix   dm;
     dm[2][2] = cos(Ex.omega) * cos(Ex.phi);
 }
 
+
+void rotation_matrix(double phi, double kappa, double omega, Dmatrix dm){
+
+    cphi = cos(phi);
+    sphi = sin(phi);
+    comega = cos(omega);
+    somega = sin(omega);
+    ckappa = cos(kappa);
+    skappa = sin(kappa);
+    
+    
+    dm[0][0] = cphi * ckappa;
+    dm[0][1] = -cphi * skappa;
+    dm[0][2] = sphi;
+    dm[1][0] = comega * skappa + somega * sphi * ckappa;
+    dm[1][1] = cos(Ex.omega) * cos(Ex.kappa)
+             - sin(Ex.omega) * sin(Ex.phi) * sin(Ex.kappa);
+    dm[1][2] = (-1) * sin(Ex.omega) * cos(Ex.phi);
+    dm[2][0] = sin(Ex.omega) * sin(Ex.kappa)
+             - cos(Ex.omega) * sin(Ex.phi) * cos(Ex.kappa);
+    dm[2][1] = sin(Ex.omega) * cos(Ex.kappa)
+             + cos(Ex.omega) * sin(Ex.phi) * sin(Ex.kappa);
+    dm[2][2] = cos(Ex.omega) * cos(Ex.phi);
+
+
+return dm;
+}
