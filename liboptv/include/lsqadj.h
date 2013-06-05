9702f160b4a0d5246c8b47997a8ba3b1d763083b
Alex Liberzon <alex.liberzon@gmail.com>
2013-06-05 22:01:24 +0300

    changed lsqadj.c to 'modern function type" with clear argument specification. updated lsqadj.h and ray_tracing.c to include "lsqadj.h"

--- liboptv/include/lsqadj.h
+++ liboptv/include/lsqadj.h
@@ -6,10 +6,15 @@
 void ata(double *a, double *ata, int m, int n);
 void ata_v2(double *a, double *ata, int m, int n, int n_large);
 void atl(double *a, double *u, double *l, int m, int n);
+void atl_v2 (double *u, double *a, double *l, int m, int n, int n_large);
+
 
 void matinv(double *a, int n);
-void matmul(double *a, double *b, double *c,
-    int m, int n, int k);
+void matinv_v2 (double *a, int n, int n_large);
+void matmul(double *a, double *b, double *c, int m, int n, int k);
+void matmul_v2 (double *a, double *b, double *c, int m,int n,int k,\
+int m_large, int n_large);
+void transp (double a[], int m, int n);
 void mat_transpose(double *mat1, double *mat2, int m, int n);
 
 #endif
