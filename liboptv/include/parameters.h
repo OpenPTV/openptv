b8950fc0170f60a5efc46ddd2bfa984c06497764
Alex Liberzon <alex.liberzon@gmail.com>
2013-06-13 20:06:44 +0300

    added parameters.{c,h} from Yosefm bitbucket repository fixed CMakeLists.txt added parameters to the test folder

---
+++ liboptv/include/parameters.h
@@ -0,0 +1,78 @@
+/* Define classes for handling parameters (storing, reading, writing) so that
+   all of libptv has a single point of entry rather than different formats in 
+   the same code.
+*/
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+/* Length of basenames. Smaller enough than the buffer lengths used by the
+   tracking framebuffer, that suffixes can be added. */
+#define SEQ_FNAME_MAX_LEN 240
+
+typedef struct {
+    char **img_base_name;
+    int first, last;
+} sequence_par;
+
+sequence_par* read_sequence_par(char *filename);
+
+typedef struct
+{
+    double  dacc, dangle, dvxmax, dvxmin;
+    double dvymax, dvymin, dvzmax, dvzmin;
+    int dsumg, dn, dnx, dny, add;
+} track_par;
+
+track_par* read_track_par(char *filename);
+int compare_track_par(track_par *t1, track_par *t2);
+
+typedef struct {
+    double X_lay[2], Zmin_lay[2], Zmax_lay[2];
+    
+    /* Criteria for correspondence are in the same file. For now they'll be
+    in the same structure, but TODO: separate them. */
+    double cn, cnx, cny, csumg, eps0, corrmin;
+} volume_par;
+
+volume_par* read_volume_par(char *filename);
+int compare_volume_par(volume_par *v1, volume_par *v2);
+
+
+
+typedef struct {
+    int  	nlay; 
+    double  n1;
+    double  n2[3];
+    double  d[3];
+    double  n3;
+    int     lut;
+} mm_np;
+
+
+/* Parameters that control general aspects in the setup and behaviour of 
+   various parts of the program, like image basenames etc. */
+typedef struct {
+    int num_cams;
+    char **img_base_name; /* Note the duplication with sequence_par. */
+    char **cal_img_base_name;
+    int hp_flag;
+    int allCam_flag;
+    int tiff_flag;
+    int imx;
+    int imy;
+    double pix_x;
+    double pix_y;
+    int chfield; 
+    mm_np *mm; 
+} control_par;
+
+
+
+
+control_par* read_control_par(char *filename);
+void free_control_par(control_par *cp);
+int compare_control_par(control_par *c1, control_par *c2);
+
+#endif
+
---
+++ liboptv/tests/check_parameters.c
@@ -0,0 +1,129 @@
+/* Unit tests for reading and writing parameter files. */
+
+#include <check.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "parameters.h"
+
+START_TEST(test_read_sequence_par)
+{
+    int cam;
+    char fname[SEQ_FNAME_MAX_LEN];
+    sequence_par *seqp;
+
+    seqp = read_sequence_par("testing_fodder/parameters/sequence.par");
+    
+    for (cam = 0; cam < 4; cam++) {
+        printf("%s", seqp->img_base_name[cam]);
+        sprintf(fname, "dumbbell/cam%d_Scene77_", cam + 1);
+        fail_unless(strncmp(fname, seqp->img_base_name[cam],
+            SEQ_FNAME_MAX_LEN - 1) == 0);
+    }
+    fail_unless(seqp->first == 497);
+    fail_unless(seqp->last == 597);
+    
+    
+    
+}
+END_TEST
+
+START_TEST(test_read_track_par)
+{
+    track_par tpar_correct = {
+        0.4, 120, 2.0, -2.0, 2.0, -2.0, 2.0, -2.0, 0., 0., 0., 0., 1. 
+    };
+    
+    track_par *tpar;
+    tpar = read_track_par("testing_fodder/parameters/track.par");
+    
+    fail_unless(compare_track_par(tpar, &tpar_correct));
+}
+END_TEST
+
+START_TEST(test_read_volume_par)
+{
+    volume_par vpar_correct = {
+        {-250., 250.}, {-100., -100.}, {100., 100.}, 0.01, 0.3, 0.3, 0.01, 1, 33
+    };
+    
+    volume_par *vpar;
+    vpar = read_volume_par("testing_fodder/parameters/criteria.par");
+    
+    fail_unless(compare_volume_par(vpar, &vpar_correct));
+}
+END_TEST
+
+START_TEST(test_read_control_par)
+{
+    int cam;
+    char img_format[] = "dumbbell/cam%d_Scene77_4085";
+    char cal_format[] = "cal/cam%d.tif";
+    control_par cpar_correct, *cpar;
+    
+    cpar_correct.num_cams = 4;
+    cpar_correct.img_base_name = (char **) malloc(4*sizeof(char *));
+    cpar_correct.cal_img_base_name = (char **) malloc(4*sizeof(char *));
+    cpar_correct.mm = (mm_np *) malloc(sizeof(mm_np));
+    
+    
+    for (cam = 0; cam < 4; cam++) {
+        cpar_correct.img_base_name[cam] = 
+            (char *) malloc((strlen(img_format) + 1) * sizeof(char));
+        sprintf(cpar_correct.img_base_name[cam], img_format, cam + 1);
+        
+        cpar_correct.cal_img_base_name[cam] = 
+            (char *) malloc((strlen(cal_format) + 1) * sizeof(char));
+        sprintf(cpar_correct.cal_img_base_name[cam], cal_format, cam + 1);
+    }
+    
+    cpar_correct.hp_flag = 1;
+    cpar_correct.allCam_flag = 0;
+    cpar_correct.tiff_flag = 1;
+    cpar_correct.imx = 1280;
+    cpar_correct.imy = 1024;
+    cpar_correct.pix_x  = 0.017;
+    cpar_correct.pix_y = 0.017;
+    cpar_correct.chfield = 0;
+    cpar_correct.mm->n1 = 1;
+    cpar_correct.mm->n2[0] = 1.49;
+    cpar_correct.mm->n3 = 1.33;
+    cpar_correct.mm->d[0] = 5;
+    
+  
+    cpar = read_control_par("testing_fodder/parameters/ptv.par");
+    fail_unless(compare_control_par(cpar, &cpar_correct));
+}
+END_TEST
+
+Suite* fb_suite(void) {
+    Suite *s = suite_create ("Parameters handling");
+
+    TCase *tc = tcase_create ("Read sequence parameters");
+    tcase_add_test(tc, test_read_sequence_par);
+    suite_add_tcase (s, tc);
+    
+    tc = tcase_create ("Read tracking parameters");
+    tcase_add_test(tc, test_read_track_par);
+    suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Read illuminated volume parameters");
+    tcase_add_test(tc, test_read_volume_par);
+    suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Read control parameters");
+    tcase_add_test(tc, test_read_control_par);
+    suite_add_tcase (s, tc);
+
+    return s;
+}
+
+int main(void) {
+    int number_failed;
+    Suite *s = fb_suite ();
+    SRunner *sr = srunner_create (s);
+    srunner_run_all (sr, CK_ENV);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
