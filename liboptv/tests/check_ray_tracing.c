44d11999dc062762c7ef64531cf3178e9e68676a
Alex Liberzon <alex.liberzon@gmail.com>
2013-08-04 00:14:07 +0300

    finished to test ray_tracing_v2 function inside ray_tracing.c

--- liboptv/tests/check_ray_tracing.c
+++ liboptv/tests/check_ray_tracing.c
@@ -8,7 +8,7 @@
 #include "calibration.h"
 #include "ray_tracing.h"
 
-#define EPS 0.001
+#define EPS 1E-5
 
 
 START_TEST(test_norm_cross)
@@ -28,15 +28,27 @@ fail_unless( (n[0] == 0.0) && (n[1] == 0.0) && (n[2] == 1.0));
 // test negative values in the output
 
 norm_cross(b,a,&n[0],&n[1],&n[2]);
-fail_unless( (n[0] == 0.0) && (n[1] == 0.0) && (n[2] == -1.0));
+// fail_unless( (n[0] == 0.0) && (n[1] == 0.0) && (n[2] == -1.0));
 
+
+ck_assert_msg( fabs(n[0] - 0.0) < EPS && 
+     		   fabs(n[1] - 0.0) < EPS && 
+     		   fabs(n[2] - -1.0)  < EPS,
+         "Was expecting n to be 0., 0., -1. but found %f %f %f\n", n[0],n[1],n[2]);
+         
 
 // our norm_cross had a bug when multiplying the parallel vectors
 // it was returning nan instead of 0.0
 // fixed Aug. 3, 2013, see in ray_tracing.c
 
 norm_cross(a,a,&n[0],&n[1],&n[2]);
-fail_unless( (n[0] == 0.0) && (n[1] == 0.0) && (n[2] == 0.0));
+// fail_unless( (n[0] == 0.0) && (n[1] == 0.0) && (n[2] == 0.0));
+
+ck_assert_msg( fabs(n[0] - 0.0) < EPS && 
+     		   fabs(n[1] - 0.0) < EPS && 
+     		   fabs(n[2] - 0.0)  < EPS,
+         "Was expecting n to be 0., 0., 0. but found %f %f %f\n", n[0],n[1],n[2]);
+
 
 }
 END_TEST
@@ -54,14 +66,18 @@ double b[] = {0.0, 2.0, 0.0};
 
 dot(a,b,&d);
 
-fail_unless( d == 0.0 );
+//fail_unless( d == 0.0 );
+ck_assert_msg( fabs(d - 0.0) < EPS,
+         "Was expecting d to be 0.0 but found %f \n", d);
 
 b[0] = 2.0;
 b[1] = 2.0;
 b[2] = 0.0;
 
 dot(b,a,&d);
-fail_unless( d == 2.0 );
+// fail_unless( d == 2.0 );
+ck_assert_msg( fabs(d - 2.0) < EPS,
+         "Was expecting d to be 2.0 but found %f \n", d);
 
 }
 END_TEST
@@ -74,7 +90,9 @@ double m;
 
 modu(a,&m);
 
-fail_unless( m == 10.0);
+// fail_unless( m == 10.0);
+ck_assert_msg( fabs(m - 10.0) < EPS,
+         "Was expecting m to be 10.0 but found %f \n", m);
 
 }
 END_TEST
@@ -86,7 +104,7 @@ START_TEST(test_matmul)
 	double a[] = {1.0,1.0,1.0};
 	double b[] = {0.0,0.0,0.0};
 		
-	Exterior test_ext = {
+	Exterior test_Ex = {
         0.0, 0.0, 100.0,
         0.0, 0.0, 0.0, 
         {{1.0, 0.2, -0.3}, 
@@ -97,7 +115,7 @@ START_TEST(test_matmul)
     // printf("b: %6.3f %6.3f %6.3f\n", b[0],b[1],b[2]);
     
     
-    matmul (b, (double *) test_ext.dm, a, 3,3,1);
+    matmul (b, (double *) test_Ex.dm, a, 3,3,1);
     
     // printf("a: %6.3f %6.3f %6.3f\n", a[0],a[1],a[2]);
     // printf("b: %6.3f %6.3f %6.3f\n", b[0],b[1],b[2]);
@@ -115,24 +133,56 @@ END_TEST
 
 START_TEST(test_ray_tracing)
 {
-	double a[] = {1.0,1.0,1.0};
-	double b[] = {0.0,0.0,0.0};
+	double     x1, y1, a,b,c, X1,Y1,Z1;
+	
+	x1 = 100.;
+	y1 = 100.;
+	
+
 		
-	Exterior test_ext = {
+	Exterior test_Ex = {
         0.0, 0.0, 100.0,
         0.0, 0.0, 0.0, 
         {{1.0, 0.2, -0.3}, 
         {0.2, 1.0, 0.0},
         {-0.3, 0.0, 1.0}}};
         
-    Interior test_int = {0., 0., 100.};
-    Glass test_glass = {0.0001, 0.00001, 1.};
-    ap_52 test_addp = {0., 0., 0., 0., 0., 1., 0.};
-        
-            
-    ray_tracing_v2 (x1,y1, test_ext, I1, G1, mmp, &X1, &Y1, &Z1, &a, &b, &c);
+    Interior test_I = {0., 0., 100.};
+    Glass test_G = {0.0001, 0.00001, 1.};
+    // ap_52 test_addp = {0., 0., 0., 0., 0., 1., 0.};
     
-    fail_unless(1 == 1);    
+/*    
+    typedef struct {
+    int  	nlay; 
+    double  n1;
+    double  n2[3];
+    double  d[3];
+    double  n3;
+    int     lut;
+} mm_np;
+ */   
+    
+    mm_np test_mmp = {
+    3,
+    1.,
+    {1.49,0.,0.},
+    {5.,0.,0.},
+    1.33,
+    1.};   
+            
+    ray_tracing_v2 (x1,y1, test_Ex, test_I, test_G, test_mmp, \
+    					&X1, &Y1, &Z1, &a, &b, &c);
+    
+    ck_assert_msg( fabs(X1 - 110.406944) < EPS && 
+     			   fabs(Y1 - 88.325788) < EPS && 
+     			   fabs(Z1 - 0.988076)  < EPS,
+         "Was expecting X1,Y1,Z1 to be 110.406944 88.325788 0.988076 but found %f %f %f\n", X1,Y1,Z1);
+    ck_assert_msg( fabs(a - 0.387960) < EPS && 
+     			   fabs(b - 0.310405) < EPS && 
+     			   fabs(c - -0.867834)  < EPS,
+         "Was expecting a,b,c to be 0.387960 0.310405 -0.867834 but found %f %f %f\n", a,b,c);     
+    
+    
     
 }
 END_TEST
