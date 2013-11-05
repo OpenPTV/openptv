3ff0ef2e9c36172b15ff51b3b66d01691507534b
acorbe <corbisoft.codes@gmail.com>
2013-11-05 16:44:53 +0100

    adopted Alessandro format for pixel_to_metric and metric_to_pixel with wrapping updated check_trafo.c to work with the new argument order (output is always first) WARNING - a lot of signatures will have to change across the code RECOMMENDED - use the wrapped version that sends control_par *cpar instead of the components imx,imy…

--- liboptv/include/trafo.h
+++ liboptv/include/trafo.h
@@ -4,15 +4,62 @@
 #include <math.h>
 #include "parameters.h"
 #include "calibration.h"
-#include "tracking_frame_buf.h"
+#include <optv/tracking_frame_buf.h>
 #include "lsqadj.h"
 
-void pixel_to_metric (double xp, double yp, int imx, int imy, double pix_x, \
-double pix_y, double *xc, double *yc, int field);
-void metric_to_pixel (double xc, double yc, int imx, int imy, double pix_x, \
-double pix_y, double *xp, double *yp, int field);
-void correct_brown_affin (double x, double y, ap_52 ap, double *x1, double *y1);
-void distort_brown_affin (double x, double y, ap_52 ap, double *x1, double *y1);
+
+/* preliminary remapping of the y pixel coordinate */
+typedef enum {
+  NO_REMAP,
+  DOUBLED_PLUS_ONE,
+  DOUBLED
+} y_remap_mode_t;
+
+
+/*  transformation detection pixel coordinates -> geometric coordinates */
+void pixel_to_metric (double * x_metric
+		       , double * y_metric
+		       , double x_pixel
+		       , double y_pixel
+		       , int im_size_x
+		       , int im_size_y
+		       , double pix_size_x
+		       , double pix_size_y
+		       , int y_remap_mode);
+
+/*  wraps previous one, parameters are read directly from control_par* structure */
+void pixel_to_metric_control_par(double * x_metric
+				 , double * y_metric
+				 , double x_pixel
+				 , double y_pixel
+				 , control_par* parameters				 
+				 );
+
+
+/*  transformation detection geometric coordinates -> pixel coordinates */
+void metric_to_pixel (double * x_pixel
+		      , double * y_pixel
+		      , double x_metric
+		      , double y_metric
+		      , int im_size_x
+		      , int im_size_y
+		      , double pix_size_x
+		      , double pix_size_y
+		      , int y_remap_mode);
+
+/* correct for Brown affine transformation */
+void correct_brown_affin (double x
+                        , double y
+                        , ap_52 ap
+                        , double *x1
+                        , double *y1);
+     
+/* distort according to affine transformation */                        
+void distort_brown_affin (double x
+                        , double y
+                        , ap_52 ap
+                        , double *x1
+                        , double *y1);
 
 
 #endif
