b8eb77f27f1e4702457bd82f37b19d9527202515
Yosef Meller <mellerf@netvision.net.il>
2013-11-19 16:01:59 +0200

    Fix rotation matrix bug found by Alex

--- liboptv/tests/check_calibration.c
+++ liboptv/tests/check_calibration.c
@@ -2,12 +2,16 @@
 /*  Unit-test suit for the C core of PyPTV. Uses the Check framework:
     http://check.sourceforge.net/
     
-    To run it, type "make check" when in the top C directory, src_c/
+    To run it, type "make verify" when in the build directory.
+    
+    References:
+    [1] Craig, J.J., Introduction to Robotics, 2nd ed.
 */
 
 #include <check.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "calibration.h"
 
@@ -29,6 +33,27 @@ Calibration test_cal(void) {
     rotation_matrix(&correct_cal.ext_par);
     
     return correct_cal;
+}
+
+/* Fill in the fields of an Exterior struct to be all zeros. */
+void zero_exterior(Exterior *ex) {
+    int i, j;
+    ex->x0 = ex->y0 = ex->z0 = ex->omega = ex->phi = ex->kappa = 0;
+    
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            ex->dm[i][j] = 0;
+}
+
+/* Compare two 3x3 matrices. Return true if all elements are equal, 
+   false otherwise
+ */
+int compare_matrix(Dmatrix m1, Dmatrix m2) {
+    int i, j;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (fabs(m1[i][j] - m2[i][j]) > 1e-6) return 0;
+    return 1;
 }
 
 /* Regression test for reading orientation files. Just reads a sample file and
@@ -68,17 +93,50 @@ START_TEST(test_write_ori)
 }
 END_TEST
 
+START_TEST(test_rotation_angles)
+{
+    Exterior ex;
+    /* Correct results may be verified using [1] */
+    Dmatrix rotx = {{1., 0., 0.}, {0., 0., -1.}, {0., 1., 0.}};
+    Dmatrix roty = {{0., 0., 1.}, {0., 1., 0.}, {-1., 0., 0.}};
+    Dmatrix rotz = {{0., -1., 0.}, {1., 0., 0.}, {0., 0., 1.}};
+    
+    /* omega */
+    zero_exterior(&ex);
+    ex.omega = M_PI/2.;
+    rotation_matrix(&ex);
+    fail_unless(compare_matrix(ex.dm, rotx));
+    
+    /* phi */
+    zero_exterior(&ex);
+    ex.phi = M_PI/2.;
+    rotation_matrix(&ex);
+    fail_unless(compare_matrix(ex.dm, roty));
+    
+    /* kappa */
+    zero_exterior(&ex);
+    ex.kappa = M_PI/2.;
+    rotation_matrix(&ex);
+    fail_unless(compare_matrix(ex.dm, rotz));
+}
+END_TEST
+
 Suite* ptv_suite(void) {
     Suite *s = suite_create ("PTV");
+    TCase *tc;
 
-    TCase *tc_rori = tcase_create ("Read orientation file");
-    tcase_add_test(tc_rori, test_read_ori);
-    suite_add_tcase (s, tc_rori);
+    tc = tcase_create ("Read orientation file");
+    tcase_add_test(tc, test_read_ori);
+    suite_add_tcase (s, tc);
 
-    TCase *tc_wori = tcase_create ("Write orientation file");
-    tcase_add_test(tc_wori, test_write_ori);
-    suite_add_tcase (s, tc_wori);
+    tc = tcase_create ("Write orientation file");
+    tcase_add_test(tc, test_write_ori);
+    suite_add_tcase (s, tc);
 
+    tc = tcase_create ("Basic rotations");
+    tcase_add_test(tc, test_rotation_angles);
+    suite_add_tcase (s, tc);
+    
     return s;
 }
 
