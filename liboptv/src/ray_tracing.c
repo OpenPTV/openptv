791bc9e7c8e6055327fb66068181c416350511d9
Alex Liberzon <alex.liberzon@gmail.com>
2014-01-09 22:07:42 +0200

    removed commented out code

--- liboptv/src/ray_tracing.c
+++ liboptv/src/ray_tracing.c
@@ -43,24 +43,6 @@ double *a3, double *b3,double *c3){
 	/* direction cosines in space coordinate system , medium n1 */
 	a1 = vect2[0];  b1 = vect2[1];  c1 = vect2[2];  
 	
-    //old d1 = -(Ex.z0 - mm.d[0]) / c1;
-    //find dist to outer interface
-	//...   from Jakob Mann vector3 XLinePlane(vector3 a, vector3 b, struct plane pl)
-    //...   a + b*((pl.c - dot(pl.base[2],a))/dot(pl.base[2],b));
-    
-	/*Ex.x0=0.;
-    Ex.y0=20.;
-    Ex.z0=10.;
-    Ex.omega=-0.7853981;
-    Ex.phi=0.;
-    Ex.kappa=0.;
-    G.vec_x=0.;
-    G.vec_y=10.;
-    G.vec_z=0.;
-	vect2[0]=0.;
-	vect2[1]=-1./sqrt(2.);
-	vect2[2]=-1./sqrt(2.);*/
-   
 	
 	a[0]=Ex.x0;a[1]=Ex.y0;a[2]=Ex.z0;
 	b[0]=vect2[0];b[1]=vect2[1];b[2]=vect2[2];
@@ -74,24 +56,10 @@ double *a3, double *b3,double *c3){
 	
 
 	/* point on the horizontal plane between n1,n2 */
-	//old Xb1 = Ex.x0 + d1*a1;  Yb1 = Ex.y0 + d1*b1;  Zb1 = Ex.z0 + d1*c1;
 	Xb1=a[0]+b[0]*d1;
 	Yb1=a[1]+b[1]*d1;
 	Zb1=a[2]+b[2]*d1;
 	
-	//old cosi1 = c1;
-	//cosi1=base2[0]*b[0]+base2[1]*b[1]+base2[2]*b[2];
-	//factor = cosi1 * mm.n1/mm.n2[0]
-	//	   + sqrt (1 - (mm.n1*mm.n1)/(mm.n2[0]*mm.n2[0])
-	//	   			 + (cosi1*cosi1)*(mm.n1*mm.n1)/(mm.n2[0]*mm.n2[0]));
-
-	/* direction cosines in space coordinate system , medium n2 */
-	//old a2 = a1 * mm.n1/mm.n2[0];
-	//old b2 = b1 * mm.n1/mm.n2[0];
-	//old c2 = c1 * mm.n1/mm.n2[0] - factor;
-	
-	//old d2 = -mm.d[0]/c2;
-
     bn[0]=base2[0];bn[1]=base2[1];bn[2]=base2[2];
 	n=(b[0]*bn[0]+b[1]*bn[1]+b[2]*bn[2]);
 	bp[0]=b[0]-bn[0]*n;bp[1]=b[1]-bn[1]*n;bp[2]=b[2]-bn[2]*n;
@@ -99,8 +67,9 @@ double *a3, double *b3,double *c3){
 	bp[0]=bp[0]/dummy;bp[1]=bp[1]/dummy;bp[2]=bp[2]/dummy;
 
 	p=sqrt(1-n*n);
-	p = p * mm.n1/mm.n2[0];//interface parallel
-	//n = n * mm.n1/mm.n2[0] - factor;//interface normal
+	/* interface parallel */
+	p = p * mm.n1/mm.n2[0];
+	/* interface normal */
 	n=-sqrt(1-p*p);
 	a2=p*bp[0]+n*bn[0];
 	b2=p*bp[1]+n*bn[1];
@@ -111,25 +80,13 @@ double *a3, double *b3,double *c3){
 	/* point on the horizontal plane between n2,n3 */
 	*Xb2 = Xb1 + d2*a2;  *Yb2 = Yb1 + d2*b2;  *Zb2 = Zb1 + d2*c2;
 	
-	//old cosi2 = c2;
-	//cosi2=base2[0]*a2+base2[1]*b2+base2[2]*c2;
-	//factor = cosi2 * mm.n2[0]/mm.n3 
-	//	   + sqrt (1 - (mm.n2[0]*mm.n2[0])/(mm.n3*mm.n3)
-	//	   			 + (cosi2*cosi2)*(mm.n2[0]*mm.n2[0])/(mm.n3*mm.n3));
-
-	/* direction cosines in space coordinate system , medium mm.n3 */
-	//old *a3 = a2 * mm.n2[0]/mm.n3;
-	//old *b3 = b2 * mm.n2[0]/mm.n3;
-	//old *c3 = c2 * mm.n2[0]/mm.n3 - factor;
-
 	n=(a2*bn[0]+b2*bn[1]+c2*bn[2]);
 	bp[0]=a2-bn[0]*n;bp[1]=b2-bn[1]*n;bp[2]=c2-bn[2]*n;
 	dummy=sqrt(bp[0]*bp[0]+bp[1]*bp[1]+bp[2]*bp[2]);
 	bp[0]=bp[0]/dummy;bp[1]=bp[1]/dummy;bp[2]=bp[2]/dummy;
 
 	p=sqrt(1-n*n);
-	p = p * mm.n2[0]/mm.n3;//interface parallel
-	//n = n * mm.n2[0]/mm.n3 - factor;//interface normal
+	p = p * mm.n2[0]/mm.n3;
 	n=-sqrt(1-p*p);
 	*a3=p*bp[0]+n*bn[0];
 	*b3=p*bp[1]+n*bn[1];
