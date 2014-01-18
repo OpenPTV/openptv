9b39f17fac16f88280c935f0f9ec93b169895aaa
Alex Liberzon <alex.liberzon@gmail.com>
2014-01-19 00:06:32 +0200

    created the first test function, need some sort of the comparison with the old one

---
+++ liboptv/tests/check_multimed.c
@@ -0,0 +1,90 @@
+/* Unit tests for ray tracing. */
+
+#include <check.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "parameters.h"
+#include "calibration.h"
+#include "lsqadj.h"
+#include "ray_tracing.h"
+#include "multimed.h"
+
+#define EPS 1E-5
+
+
+START_TEST(test_trans_Cam_Point_back)
+{
+    /* input */
+    double x = 100.0;
+    double y = 100.0;
+    double z =  0.0;        
+        
+    Exterior test_Ex = {
+        0.0, 0.0, 100.0,
+        0.0, 0.0, 0.0, 
+        {{1.0, 0.2, -0.3}, 
+        {0.2, 1.0, 0.0},
+        {-0.3, 0.0, 1.0}}};
+    
+    Interior test_I = {0.0, 0.0, 100.0};
+    Glass test_G = {0.0001, 0.00001, 1.0};
+    ap_52 test_addp = {0., 0., 0., 0., 0., 1., 0.};
+    Calibration test_cal = {test_Ex, test_I, test_G, test_addp};
+    
+    mm_np test_mm = {
+    	3, 
+    	1.0, 
+    	{1.49, 0.0, 0.0}, 
+    	{5.0, 0.0, 0.0},
+    	1.33,
+    	1};
+    
+    /* output */
+    Exterior Ex_t; 
+    double X_t, Y_t, Z_t;
+    double cross_p[3], cross_c[3]; 
+
+     printf("Entered test_trans_Cam_Point_back \n");
+     trans_Cam_Point_back(test_Ex, test_mm, test_G, x, y, z, &Ex_t, &X_t, &Y_t, &Z_t, \
+     &cross_p, &cross_c);
+    
+    
+     ck_assert_msg( fabs(X_t - 141.429134) < EPS && 
+                    fabs(Y_t - 0.0) < EPS && 
+                    fabs(Z_t + 5.991000)  < EPS,
+         "Expected 141.429134 0.000000 -5.991000  but found %f %f %f\n", X_t, Y_t, Z_t);
+      
+      
+         
+     ck_assert_msg( fabs(cross_p[0] - 100.000099) < EPS && 
+                    fabs(cross_p[1] - 100.000010) < EPS && 
+                    fabs(cross_p[2] - 0.991000)  < EPS,
+         "Expected 100.0 100.0 0.991 but found %f %f %f\n", cross_p[0],cross_p[1],cross_p[2]);
+  
+    
+}
+END_TEST
+
+
+Suite* fb_suite(void) {
+    Suite *s = suite_create ("multimed");
+ 
+    TCase *tc = tcase_create ("multimed_test");
+    tcase_add_test(tc, test_trans_Cam_Point_back);
+    suite_add_tcase (s, tc);   
+    return s;
+}
+
+int main(void) {
+    int number_failed;
+    Suite *s = fb_suite ();
+    SRunner *sr = srunner_create (s);
+    //srunner_run_all (sr, CK_ENV);
+    //srunner_run_all (sr, CK_SUBUNIT);
+    srunner_run_all (sr, CK_VERBOSE);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
