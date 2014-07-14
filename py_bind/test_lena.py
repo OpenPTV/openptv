980db12917a49a16a3deae1406191728b0d6d270
Alex Liberzon <alex.liberzon@gmail.com>
2014-07-14 23:12:24 +0300

    changed the way to call <unsigned char*> img1.data instead of contiguous arrays and pointer to &img1[0,0]

--- py_bind/test_lena.py
+++ py_bind/test_lena.py
@@ -2,14 +2,13 @@ from matplotlib.pylab import *
 from skimage.data import lena
 import numpy as np
 
-# a = np.ascontiguousarray(lena()[:,:,0]).astype(np.uint8)
+from optv.image_processing import py_lowpass_3
+
+
 a = lena()[:,:,0].astype(np.uint8)
 b = np.copy(a)
-
-from optv.image_processing import lowpass_3
-
-
-lowpass_3(a, b)
+imx,imy = a.shape
+py_lowpass_3(a,b,imx*imy,imx) 
 
 imshow(np.c_[a,b],cmap='gray')
 show()
@@ -17,8 +16,26 @@ show()
 imshow(a-b,cmap='gray')
 show()
 
-a = np.ascontiguousarray(8*np.ones((3,3),dtype=np.uint8))
+a = 200*np.ones((3,3),dtype=np.uint8)
 b = np.copy(a)
-lowpass_3(a,b) 
+imx,imy = a.shape
+py_lowpass_3(a,b,imx*imy,imx) 
 print a
 print b
+
+
+a = np.ones((3,6),dtype=np.uint8)
+b = np.copy(a)
+imx,imy = a.shape
+py_lowpass_3(a,b, imx*imy, imx) 
+print a
+print b
+
+
+a = 200*np.ones((6,3),dtype=np.uint8)
+b = np.copy(a)
+imx,imy = a.shape
+py_lowpass_3(a,b, imx*imy, imx) 
+print a
+print b
+
