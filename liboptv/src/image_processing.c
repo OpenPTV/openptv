5f98e22a35d0bb9e24df6953ac580fa8dda5f4b6
Yosef Meller <mellerf@netvision.net.il>
2015-08-02 19:36:14 +0300

    Add box-blur (previously unsharp_mask) to image processing code.

--- liboptv/src/image_processing.c
+++ liboptv/src/image_processing.c
@@ -7,6 +7,7 @@
  *
  ***************************************************************************/
 
+#include <stdlib.h>
 #include "image_processing.h"
 
 /*  This would be a function, only the original writer of these filters put a 
@@ -104,4 +105,143 @@ void lowpass_3(unsigned char *img, unsigned char *img_lp, control_par *cpar) {
         *ptr++ = buf/9;
     }
 }
+
+
+/*  fast_box_blur() performs a box blur of an image using a given kernel size.
+    It is equivalent to using an all-ones kernel of the given size in a 
+    function like filter_3 (but adjusted to size). However, this algorithm runs
+    in linear filter-size time instead of quadratic (it's still quadratic in 
+    image size).
+    
+    Arguments:
+    int filt_span - how many pixels to take for the average on each side. The
+        equivalent filter kernel is of side 2*filt_size + 1.
+    unsigned char *src - points to the source image.
+    unsigned char *dest - points to same-size memory block allocated for the 
+        result.
+    control_par *cpar - contains image size parameters.
+    
+    Returns:
+    0 on failure (due to memory allocation error), 1 on success.
+    
+    References:
+    [1] http://www.filtermeister.com/tutorials/blur01.html
+    [2] http://www.gamasutra.com/view/feature/131511/four_tricks_for_fast_blurring_in_.php
+*/
+int fast_box_blur(int filt_span, unsigned char *src, unsigned char *dest, 
+    control_par *cpar) 
+{
+    register unsigned char  *ptrl, *ptrr, *ptrz;
+    register int *ptr, *ptr1, *ptr2, *ptr3;
+    int *row_accum, *col_accum, accum, *end;
+    int row_start, n, nq, m;
+    register int i;
+    int image_size = cpar->imx * cpar->imy;
+    
+    n = 2*filt_span + 1;
+    nq = n*n;
+    
+    if ((row_accum = (int *)calloc(image_size, sizeof(int))) == NULL) return 0;
+    if ((col_accum = (int *)calloc(cpar->imx, sizeof(int))) == NULL) return 0;
+    
+    /* Sum over lines first [1]: */
+    for (i = 0; i < cpar->imy; i++) {
+        row_start = i * cpar->imx;
+        
+        /* first element has no filter around him */
+        accum = *(src + row_start);
+        *(row_accum + row_start) = accum * n;
+        
+        /* Elements 1..filt_span have a growing filter, as much as fits.
+           Each iteration increases the filter symmetrically, so 2 new elements 
+           are taken.
+        */
+        for (ptr = row_accum + row_start + 1, 
+            ptrr = src + row_start + 2, ptrl = ptrr - 1, m = 3;
+            ptr < row_accum + row_start + 1 + filt_span;
+            ptr++, ptrl+=2, ptrr+=2, m+=2) 
+        {    
+            accum += (*ptrl + *ptrr);
+            *ptr = accum * n / m; /* So small filters have same weight as
+                                         the largest size */
+        }
+        
+        /* Middle elements, having a constant-size filter. The sum is obtained
+           by adding the coming element and dropping the leaving element, in a 
+           sliding window fashion. 
+        */
+        for (ptr = row_accum + row_start + filt_span + 1,
+            ptrl = src + row_start, ptrr = src + row_start + n;
+            ptrr < src + row_start + cpar->imx;
+            ptrl++, ptr++, ptrr++)
+        {
+            accum += (*ptrr - *ptrl);
+            *ptr = accum;
+        }
+        
+        /* last elements in line treated like first ones, mutatis mutandis */
+        for (ptrl = src + row_start + cpar->imx - n,
+            ptrr = ptrl + 1, m = n - 2,
+            ptr = row_accum + row_start + cpar->imx - filt_span;
+            ptr < row_accum + row_start + cpar->imx;
+            ptrl += 2, ptrr += 2, ptr++, m-=2)
+        {
+            accum -= (*ptrl + *ptrr);
+            *ptr = accum * n / m;
+        }
+    }
+    
+    /* Sum over columns: */
+    
+    end = col_accum + cpar->imx;
+    
+    /* first line */
+    for (ptr1 = row_accum, ptr2 = col_accum, ptrz = dest; 
+        ptr2 < end; ptr1++, ptr2++, ptrz++)
+    {
+       *ptr2 = *ptr1;
+       *ptrz = *ptr2/n;
+    }
+    
+    /* Sum vertically the accumulated row values for lines 1 ... filt_span */
+    for (i = 1; i <= filt_span; i++) {
+        ptr1 = row_accum + (2*i - 1)*cpar->imx;
+        ptr2 = ptr1 + cpar->imx;
+        ptrz = dest + i*cpar->imx;
+        
+        for (ptr3 = col_accum; ptr3 < end; ptr1++, ptr2++, ptr3++, ptrz++) {
+            *ptr3 += (*ptr1 + *ptr2);
+            *ptrz = n * (*ptr3) / nq / (2*i + 1);
+        }
+    }
+    
+    /* Middle lines with filter-size lines around them */
+    for (i = n+1, ptr1 = row_accum, ptrz = dest + cpar->imx*(n + 1), 
+        ptr2 = row_accum + cpar->imx*n; i < cpar->imy - n; i++)
+    {
+        for (ptr3 = col_accum; ptr3 < end; ptr3++, ptr1++, ptrz++, ptr2++) {
+           *ptr3 += (*ptr2 - *ptr1);
+           *ptrz = *ptr3/nq;
+        }
+    }
+    
+    /* Last lines, similarly to first lines */
+    for (i = n; i > 0; i--) {
+        ptr1 = row_accum + (cpar->imy - 2*i - 1)*cpar->imx;
+        ptr2 = ptr1 + cpar->imx;
+        ptrz = dest + (cpar->imy-i)*cpar->imx;
+        
+        for (ptr3 = col_accum; ptr3 < end; ptr1++, ptr2++, ptr3++, ptrz++) {
+            *ptr3 -= (*ptr1 + *ptr2);
+            *ptrz = n * (*ptr3) / nq / (2*i+1);
+        }
+    }
+    
+    /* ``dest`` now contains result. Finish up. */
+    free(row_accum);
+    free(col_accum);
+    
+    return 1;
+}
+
 
