d8eb624294b7d8bdc1cbba02275857774d79566d
Yosef Meller <mellerf@netvision.net.il>
2015-08-04 15:47:53 +0300

    Complete missing signatures in image_processing.h

--- liboptv/include/image_processing.h
+++ liboptv/include/image_processing.h
@@ -11,6 +11,9 @@ typedef double filter_t[3][3];
 int filter_3(unsigned char *img, unsigned char *img_lp, filter_t filt,
     control_par *cpar);
 void lowpass_3(unsigned char *img, unsigned char *img_lp, control_par *cpar);
+int fast_box_blur(int filt_span, unsigned char *src, unsigned char *dest, 
+    control_par *cpar);
+void split(unsigned char *img, int half_selector, control_par *cpar);
 
 #endif
 
