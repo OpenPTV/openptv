38de4bbb407730f37695eb48eb18bc97450628c5
Yosef Meller <mellerf@netvision.net.il>
2015-11-17 13:59:18 +0200

    A little bit of cleanup, mostly doc.

--- liboptv/src/ray_tracing.c
+++ liboptv/src/ray_tracing.c
@@ -1,49 +1,35 @@
 /***************************************************************
 Routine:                ray_tracing
 Author/Copyright:       Hans -  Gerd Maas
-Address:                Institute of Geodesy and Photogrammetry
-                        ETH  -   Hoenggerberg
-                        CH  -   8093 Zurich
-Creation Date:          21.4.88
-    
-Description:            traces one ray, given by image coordinates,
-                    exterior and interior orientation 
-                    through dufferent media
-                    (see Hoehle, Manual of photogrammetry)
-    
-Routines contained:      -  
 Updated for liboptv: Alex Liberzon
 Modification Date:     April 20, 2014
+
+References:
+[1] HOEHLE
+[2] Manual of Photogrammetry
 ***************************************************************/
 
 #include <stdio.h>
 #include "ray_tracing.h"
 
-
-#define EPS 1e-5
-
-
-/* ray_tracing () traces the optical ray through the multi-media interface of 
-   (presently) three layers, typically air - glass - water, and returns the two vectors: 
-    position of the ray crossing point and the vector normal to the interface 
+/*  ray_tracing () traces the optical ray through the multi-media interface of
+    (presently) three layers, typically air - glass - water, and returns the
+    position of the ray crossing point and the vector normal to the interface.
+    See refs. [1,2]
+    
     Arguments:
-    x,y - doubles, position of a point in the image space 
-    Calibration *cal structure of a specific camera
-    mm_np mm structure of the multi-media information (thickness, index of refraction)
-    Output:
-    X - vector (vec3d) of the crossing point position
-    out - vector pointing normal to the interface 
+    double x, y - metric position of a point in the image space 
+    Calibration *cal - parameters of a specific camera.
+    mm_np mm - multi-media information (thickness, index of refraction)
+    
+    Output Arguments:
+    vec3d X - crossing point position.
+    vec3d out - vector pointing normal to the interface.
 */
 
-void ray_tracing (double x
-				, double y
-				, Calibration* cal
-				, mm_np mm
-				, vec3d X
-				, vec3d out){
-
-/*   ray -  tracing, see HOEHLE and Manual of Photogrammetry  */
-
+void ray_tracing (double x, double y, Calibration* cal, mm_np mm, vec3d X,
+    vec3d out)
+{
     double d1, d2, c, dummy, n, p;
     vec3d vect1, vect2, a, base2, bn, bp, tmp1, tmp2, Xb, a2;
     
@@ -63,12 +49,10 @@ void ray_tracing (double x
     
     dummy = vec_dot(base2, a) - c;
     d1 = -1*dummy/vec_dot(base2, vect2); 
-        
 
     /*   point on the horizontal plane between n1,n2  */
     vec_scalar_mul(vect2, d1, tmp1);
     vec_add(a, tmp1, Xb);
-
         
     vec_copy(bn, base2); 
     n = vec_dot(vect2, bn);
@@ -76,7 +60,6 @@ void ray_tracing (double x
     vec_scalar_mul(bn, n, tmp1);
     vec_subt(vect2, tmp1, tmp2);
     unit_vector(tmp2, bp);
-    
 
     p = sqrt(1 -  n *  n);
     /*   interface parallel  */
@@ -84,12 +67,10 @@ void ray_tracing (double x
     /*   interface normal  */
     n =  -sqrt(1 -  p * p);
     
-    
     vec_scalar_mul(bp, p, tmp1); 
     vec_scalar_mul(bn, n, tmp2); 
     vec_add(tmp1, tmp2, a2); 
     d2 = mm.d[0]/fabs(vec_dot(base2, a2));        
-    
 
     /*   point on the horizontal plane between n2,n3  */
     vec_scalar_mul(a2, d2, tmp1);
