0af041b3a62fa1169bc71ab468f97a8498953503
Yosef Meller <mellerf@netvision.net.il>
2015-12-13 11:25:10 +0200

    Export the vec_cross function from vec_utils.h

--- liboptv/include/vec_utils.h
+++ liboptv/include/vec_utils.h
@@ -22,6 +22,7 @@ void vec_scalar_mul(vec3d vec, double scalar, vec3d output);
 double vec_norm(vec3d vec);
 double vec_diff_norm(vec3d vec1, vec3d vec2);
 double vec_dot(vec3d vec1, vec3d vec2);
+void vec_cross(vec3d vec1, vec3d vec2, vec3d out);
 int vec_cmp(vec3d vec1, vec3d vec2);
 int vec_approx_cmp(vec3d vec1, vec3d vec2, double eps);
 void unit_vector(vec3d vec, vec3d out);
