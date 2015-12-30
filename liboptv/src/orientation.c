c603bbbd79fcad200a0803fa91b92db1f6efff06
Yosef Meller <mellerf@netvision.net.il>
2015-12-30 14:13:21 +0200

    Remove scaffold ray_distance()

--- liboptv/src/orientation.c
+++ liboptv/src/orientation.c
@@ -9,35 +9,6 @@
 
 #define PT_UNUSED -999
 
-/*  ray_distance() calculates the minimal distance between skew rays.
-    Reference for algorithm: 
-    http://mathworld.wolfram.com/Line-LineDistance.html
-    
-    Arguments:
-    vec3d vert1, direct1 - vertex and direction unit vector of one ray.
-    vec3d vert2, direct2 - vertex and direction unit vector of other ray.
-*/
-double ray_distance(vec3d vert1, vec3d direct1, vec3d vert2, vec3d direct2) {
-    vec3d perp_both, sp_diff, proj;
-    double scale;
-    
-    /* vector between starting points */
-    vec_subt(vert2, vert1, sp_diff);
-    
-    /* Check for parallel rays. Then use planar geometry. */
-    if (vec_cmp(direct1, direct2)) {
-        scale = vec_dot(sp_diff, direct1);
-        vec_scalar_mul(direct1, scale, proj);
-        vec_subt(sp_diff, proj, perp_both);
-        return vec_norm(perp_both);
-    }
-    
-    /* The shortest distance is on a line perpendicular to both rays. */
-    vec_cross(direct1, direct2, perp_both);
-    
-    return fabs(vec_dot(sp_diff, perp_both))/vec_norm(perp_both);
-}
-
 /*  skew_midpoint() finds the middle of the minimal distance segment between 
     skew rays. Undefined for parallel rays.
     
