d191903eafff7d012522f3a09731610f7e87a126
segalmax <segalmax@gmail.com>
2016-02-16 18:30:03 +0200

    add *brown_affin bindings and tests. TESTS ARE NOT RELIABLE.

--- py_bind/optv/transforms.pyx
+++ py_bind/optv/transforms.pyx
@@ -1,6 +1,7 @@
 import numpy as np
 cimport numpy as np
 from optv.parameters cimport ControlParams
+from optv.calibration cimport Calibration
 
 def convert_arr_pixel_to_metric(np.ndarray[ndim=2, dtype=np.float_t] input,
                                 ControlParams control,
@@ -13,7 +14,7 @@ def convert_arr_pixel_to_metric(np.ndarray[ndim=2, dtype=np.float_t] input,
     
     @return: if no array was passed for output returns a new numpy ndarray with converted coordinates
     '''
-    return convert_generic(input, control, out, pixel_to_metric)
+    return convert_generic(input, control._control_par, out, pixel_to_metric)
   
 def convert_arr_metric_to_pixel(np.ndarray[ndim=2, dtype=np.float_t] input,
                                 ControlParams control,
@@ -26,10 +27,10 @@ def convert_arr_metric_to_pixel(np.ndarray[ndim=2, dtype=np.float_t] input,
     
     @return: if no array was passed for output returns a new numpy ndarray with converted coordinates
     '''
-    return convert_generic(input, control, out, metric_to_pixel)
+    return convert_generic(input, control._control_par, out, metric_to_pixel)
 
 cdef convert_generic(np.ndarray[ndim=2, dtype=np.float_t] input,
-                        ControlParams control,
+                        control_par * c_control,
                         np.ndarray[ndim=2, dtype=np.float_t] out,
                         void convert_function(double * , double * , double, double , control_par *)):
     # Raise exception if received non Nx2 shaped ndarray
@@ -51,6 +52,60 @@ cdef convert_generic(np.ndarray[ndim=2, dtype=np.float_t] input,
                         , < double *> np.PyArray_GETPTR2(out, i, 1)
                         , (< double *> np.PyArray_GETPTR2(input, i, 0))[0]
                         , (< double *> np.PyArray_GETPTR2(input, i, 1))[0]
-                        , control._control_par)
+                        , c_control)
     return out
-         
+
+# Affine #######################################################################
+
+#TODO naming
+def correct_arr_brown_affine(np.ndarray[ndim=2, dtype=np.float_t] input,
+                                Calibration calibration,
+                                np.ndarray[ndim=2, dtype=np.float_t] out=None):
+    '''
+    Correct crd to geo with Brown + affine.
+    @param input: input Numpy ndarray of Nx2 shape.
+    @param control: Calibration object that holds parameters needed for transformation.
+    @param out: OPTIONAL numpy ndarray, same shape as input.
+    
+    @return: if no array was passed for output - returns a new numpy ndarray with converted coordinates
+    '''
+    return brown_affine_generic(input, calibration._calibration.added_par, out, correct_brown_affin)
+  
+def distort_arr_brown_affine_(np.ndarray[ndim=2, dtype=np.float_t] input,
+                                Calibration calibration,
+                                np.ndarray[ndim=2, dtype=np.float_t] out=None):
+    '''
+    Transformation with Brown + affine. 
+    @param input: input Numpy ndarray of Nx2 shape.
+    @param control: Calibration object that holds parameters needed for transformation.
+    @param out: OPTIONAL Numpy ndarray, same shape as input.
+    
+    @return: if no array was passed for output - returns a new numpy ndarray with converted coordinates
+    '''
+    return brown_affine_generic(input, calibration._calibration.added_par, out, distort_brown_affin)
+
+cdef brown_affine_generic(np.ndarray[ndim=2, dtype=np.float_t] input,
+                        ap_52 c_ap_52,
+                        np.ndarray[ndim=2, dtype=np.float_t] out,
+                        void affine_function(double, double, ap_52 , double * , double *)):
+    # Raise exception if received non Nx2 shaped ndarray
+    # or if output and input arrays' shapes do not match.
+    if input.shape[1] != 2 or (out != None and out.shape[1] != 2):
+        raise TypeError("Only two-column matrices accepted for conversion.")
+    if out != None:
+        if not(input.shape[0] == out.shape[0] and input.shape[1] == out.shape[1]):
+            raise TypeError("Unmatching shape of input and output arrays: (" 
+                            + str(input.shape[0]) + "," + str(input.shape[1]) 
+                            + ") != (" + str(out.shape[0]) + "," + str(out.shape[1]) + ")")
+    else:
+        # If no array for output was passed (out==None):
+        # create new array for output with  same shape as input array 
+        out = np.empty_like(input)
+
+    for i in range(input.shape[0]):
+        affine_function((< double *> np.PyArray_GETPTR2(input, i, 0))[0]
+                        , (< double *> np.PyArray_GETPTR2(input, i, 1))[0]
+                        , c_ap_52
+                        , < double *> np.PyArray_GETPTR2(out, i, 0)
+                        , < double *> np.PyArray_GETPTR2(out, i, 1))
+    return out
--- py_bind/test/test_trafo_bindings.py
+++ py_bind/test/test_trafo_bindings.py
@@ -1,6 +1,7 @@
 import unittest
 from optv.parameters import *
 from optv.transforms import *
+from optv.calibration import *
 import numpy as np, os, filecmp, shutil
 
 class Test_transforms(unittest.TestCase):
@@ -10,7 +11,13 @@ class Test_transforms(unittest.TestCase):
         self.control = ControlParams(4)      
         self.control.read_control_par(self.input_control_par_file_name)
         
-    def test_go(self):
+        self.input_ori_file_name = "testing_fodder/calibration/cam1.tif.ori"
+        self.input_add_file_name = "testing_fodder/calibration/cam2.tif.addpar"
+       
+        self.calibration = Calibration()
+        self.calibration.from_file(self.input_ori_file_name, self.input_add_file_name)
+        
+    def test_transforms(self):
         # Assert TypeError is raised when passing a non (n,2) shaped numpy ndarray
         with self.assertRaises(TypeError):
             list = [[0 for x in range(2)] for x in range(10)]  # initialize a 10x2 list (but not numpy matrix)
@@ -18,9 +25,9 @@ class Test_transforms(unittest.TestCase):
         with self.assertRaises(TypeError):
             convert_arr_pixel_to_metric(np.empty((10, 3)), self.control, out=None)
         with self.assertRaises(TypeError):
-            convert_arr_pixel_to_metric(np.empty((2, 1)), self.control, out=None)
+            convert_arr_metric_to_pixel(np.empty((2, 1)), self.control, out=None)
         with self.assertRaises(TypeError):
-            convert_arr_pixel_to_metric(np.zeros((11, 2)), self.control, out=np.zeros((12, 2)))
+            convert_arr_metric_to_pixel(np.zeros((11, 2)), self.control, out=np.zeros((12, 2)))
         
         input = np.full((3, 2), 100)
         output = np.zeros((3, 2))
@@ -44,8 +51,42 @@ class Test_transforms(unittest.TestCase):
         output = np.zeros((3, 2))
         output=convert_arr_metric_to_pixel(input, self.control, out=None)
         np.testing.assert_array_almost_equal(output, correct_output_metric_to_pixel, decimal=7)
-
-            
+        
+    def test_brown_affine(self):
+        # Assert TypeError is raised when passing a non (n,2) shaped numpy ndarray
+        with self.assertRaises(TypeError):
+            list = [[0 for x in range(2)] for x in range(10)]  # initialize a 10x2 list (but not numpy matrix)
+            correct_arr_brown_affine(list, self.calibration, out=None)
+        with self.assertRaises(TypeError):
+            correct_arr_brown_affine(np.empty((10, 3)), self.calibration, out=None)
+        with self.assertRaises(TypeError):
+            distort_arr_brown_affine_(np.empty((2, 1)), self.calibration, out=None)
+        with self.assertRaises(TypeError):
+            distort_arr_brown_affine_(np.zeros((11, 2)), self.calibration, out=np.zeros((12, 2)))
+        
+        input = np.full((3, 2), 100)
+        output = np.zeros((3, 2))
+        correct_output_corr = [[ 100.,  100.],  #TODO!!
+                               [ 100.,  100.],
+                               [ 100.,  100.]]
+        correct_output_dist= [[ 100.,  100.],
+                               [ 100.,  100.],
+                               [ 100.,  100.]]
+        
+        # Test when passing an array for output
+        correct_arr_brown_affine(input, self.calibration, out=output)
+#         np.testing.assert_array_almost_equal(output, correct_output_corr,decimal=7)
+        output = np.zeros((3, 2))
+        distort_arr_brown_affine_(input, self.calibration, out=output)
+#         np.testing.assert_array_almost_equal(output, correct_output_dist, decimal=7)
+        
+         # Test when NOT passing an array for output
+        output=correct_arr_brown_affine(input, self.calibration, out=None)
+        np.testing.assert_array_almost_equal(output, correct_output_corr,decimal=7)
+        output = np.zeros((3, 2))
+        output=distort_arr_brown_affine_(input, self.calibration, out=None)
+        np.testing.assert_array_almost_equal(output, correct_output_dist, decimal=7)
+        
 if __name__ == '__main__':
   unittest.main()
  
