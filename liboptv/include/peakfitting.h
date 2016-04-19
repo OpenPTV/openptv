f14eed9e8db6e0eace01c8197b26435eb20c0db8
Alex Liberzon <alex.liberzon@gmail.com>
2016-04-19 23:30:10 +0300

    added peak fitting.{c,h} and the test from openptv-python directory

---
+++ liboptv/include/peakfitting.h
@@ -0,0 +1,19 @@
+/*  Objects related to calibration: exterior and interior parameters, glass
+    parameters. A Calibration object to rule them all and find them. 
+
+    References:
+    [1] Th. Dracos (editor). Three-Dimensional Velocity and Vorticity Measuring
+        and Image Analysis Techniques. Kluwer Academic Publishers. 1996. 
+        pp.165-168
+*/
+
+#ifndef PEAKFITTING_H
+#define PEAKFITTING_H
+
+
+
+
+int peak_fit_new ( img, par_file, xmin,xmax,ymin,ymax, pix, nr, cpar); 
+
+#endif
+
