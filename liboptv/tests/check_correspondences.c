ebeadf3ed012a49aeaa1b070edfd0aa7de545df9
Alex Liberzon <alex.liberzon@gmail.com>
2016-08-20 20:57:15 +0300

    imported test from @yosefm branch

--- liboptv/tests/check_correspondences.c
+++ liboptv/tests/check_correspondences.c
@@ -122,6 +122,78 @@ START_TEST(test_quicksort_con)
 }
 END_TEST
 
+START_TEST(test_correspondences)
+{
+    frame frm;
+    Calibration *calib[4];
+    volume_par *vpar;
+    control_par *cpar;
+    n_tupel **corres_lists;
+    mm_np media_par = {1, 1., {1., 0., 0.}, {1., 0., 0.}, 1., 1.};
+    vec3d tmp;
+
+    
+    char ori_tmpl[] = "cal/sym_cam%d.tif.ori";
+    char ori_name[25]; 
+    
+    int num_cams = 4, cam, cpt_vert, cpt_horz, cpt_ix;
+    target *targ;
+    
+    int subset_size, num_corres;
+    n_tupel *corres;
+    
+    ck_abort_msg("Known failure: j/p2 in find_candidate_plus breaks this.");
+    chdir("testing_fodder/");
+    init_proc_c();
+    
+    int i,j;
+    /* Four cameras on 4 quadrants looking down into a calibration target.
+       Calibration taken from an actual experimental setup */
+    frame_init(&frm, num_cams, 16);
+    for (cam = 0; cam < num_cams; cam++) {
+        sprintf(ori_name, ori_tmpl, cam + 1);
+        calib[cam] = read_calibration(ori_name, "cal/cam1.tif.addpar", NULL);
+        
+        
+        frm.num_targets[cam] = 16;
+        
+        /* Construct a scene representing a calibration target, generate
+           tergets for it, then use them to reconstruct correspondences. */
+        for (cpt_horz = 0; cpt_horz < 4; cpt_horz++) {
+            for (cpt_vert = 0; cpt_vert < 4; cpt_vert++) {
+                cpt_ix = cpt_horz*4 + cpt_vert;
+                if (cam % 2) cpt_ix = 15 - cpt_ix; /* Avoid symmetric case */
+                
+                targ = &(frm.targets[cam][cpt_ix]);
+                targ->pnr = cpt_ix;
+                
+                vec_set(tmp, cpt_vert * 10, cpt_horz * 10, 0);
+                img_coord(tmp, calib[cam], &media_par, &(targ->x), &(targ->y));
+                metric_to_pixel(&(targ->x), &(targ->y), targ->x, targ->y, cpar);
+                
+                /* These values work in check_epi, so used here too */
+                targ->n = 25;
+                targ->nx = targ->ny = 5;
+                targ->sumg = 10;
+            }
+        }
+    }
+    vpar = read_volume_par("parameters/criteria.par");
+    corres_lists = correspondences(&frm, calib, vpar, cpar);
+    fail_unless(corres_lists[0] == NULL);
+    
+    for (subset_size = 2; subset_size <= num_cams; subset_size++) {
+        corres = corres_lists[subset_size - 1];
+        num_corres = 0;
+        
+        while (corres->corr >= 0) {
+            num_corres++;
+            corres++;
+        }
+        fail_unless(num_corres == ((subset_size == 4) ? 16 : 0));
+    }
+}
+END_TEST
 
 
 Suite* orient_suite(void) {
@@ -142,6 +214,11 @@ Suite* orient_suite(void) {
     tc = tcase_create ("quicksort_con");
     tcase_add_test(tc, test_quicksort_con);
     suite_add_tcase (s, tc);
+    
+    tc = tcase_create ("Calibration target correspondences");
+    tcase_add_test(tc, test_correspondences);
+    suite_add_tcase (s, tc);
+    
     return s;
 }
 
