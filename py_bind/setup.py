045212f6d1cc9954c46fee975a78b56fb9c4f870
Itay Zandbank <itay@chelem.co.il>
2019-02-16 01:30:07 +0200

    Get setup to work properly on linux - wheel and source

--- py_bind/setup.py
+++ py_bind/setup.py
@@ -4,23 +4,67 @@ from distutils.core import setup
 import setuptools
 import os
 import shutil
+import sys
+import glob
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
 
-# We need to have Cython and Numpy installed.
-# setup_requires does not play nice with setup_requires, so we need this hack. There are better hacks that
-# work better (we need to check conda), but this may not be needed
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Distutils.extension import Extension
-except ImportError:
-    os.system('pip install cython')
-    from Cython.Distutils import build_ext
-    from Cython.Distutils.extension import Extension
 
-try:
-    import numpy as np
-except ImportError:
-    os.system('pip install numpy')
-    import numpy as np
+class PrepareCommand(setuptools.Command):
+    # We must make some preparations before we can build the extension.
+    # First, we should copy the liboptv sources to a subdirectory, so they can be included with the sdist package.
+    # Second, we convert the pyx files to c files, so the package can be installed from source without requiring Cython
+    description = "Copy the liboptv sources and convert pyx files to C before building"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.copy_source_files()
+        self.convert_to_c()
+
+    def copy_source_files(self):
+        if not os.path.exists('../liboptv'):
+            print('../liboptv does not exist. You must run setup.py prepare from with the full liboptv repository',
+                  file=sys.stderr)
+            raise Exception('.//liboptv does not exist')
+        
+        print('Copying the liboptv source files...')
+        if os.path.exists('./liboptv'):
+            shutil.rmtree('./liboptv')
+        os.makedirs('./liboptv')
+        shutil.copytree('../liboptv/include', 'liboptv/include/optv')
+        shutil.copytree('../liboptv/src', 'liboptv/src')
+
+    def convert_to_c(self):
+        print('Converting pyx files to C sources...')
+        pyx_files = glob.glob('./optv/*.pyx')
+        for pyx in pyx_files:
+            self.cython(pyx)
+
+    def cython(self, pyx):
+        from Cython.Compiler.CmdLine import parse_command_line
+        from Cython.Compiler.Main import compile
+        options, sources = parse_command_line(['-2', pyx])
+        result = compile(sources, options)
+        if result.num_errors > 0:
+            print('Errors converting %s to C' % pyx, file=sys.stderr)
+            raise Exception('Errors converting %s to C' % pyx)
+        self.announce('Converted %s to C' % pyx)
+
+
+class BuildExt(build_ext, object):
+    def run(self):
+        if not os.path.exists('./liboptv') or not glob.glob('./optv/*.c'):
+            print('You must run setup.py prepare before building the extension', file=sys.stderr)
+            raise Exception('You must run setup.py prepare before building the extension')
+        super(BuildExt, self).run()
+
+        # We inherite from object to make super() work, see here: https://stackoverflow.com/a/18392639/871910
 
 # The python bindings have been redone, so they do not require the liboptv.so to be installed.
 # We do the following:
@@ -33,70 +77,52 @@ except ImportError:
 # 
 # Tell Cython to look for header files in c-src/include/ (for the Cython code) and c-src/include/optv (for the C code) 
 
-def copy_optv_sources():
-    if not os.path.exists('../liboptv'):
-        # This code is running from setup.py without the surrounding repository.
-        # c-src should already be there
-        return
-    # Copy ../lipoptv/src aand .,/liboptv/inlucode to ./c-src/
-    if os.path.exists('./c-src'):
-        shutil.rmtree('./c-src')
-    os.makedirs('./c-src')
-    shutil.copytree('../liboptv/include', 'c-src/include/optv')
-    shutil.copytree('../liboptv/src', 'c-src/src/optv')
+def get_include_dirs():
+    import numpy as np
+    inc_dirs = [np.get_include(), '.', './liboptv/include', './liboptv/include/optv']
+    return inc_dirs
 
-inc_dirs = [np.get_include(), '.', './c-src/include', './c-src/include/optv']
+def get_liboptv_sources():
+    return glob.glob('./liboptv/src/*.c')
 
-def gather_c_sources():
-    # Return all the C files in the liboptv/src directory.
-    c_folder = './c-src/src'
-    all_files = os.listdir(c_folder)
-    c_files = [file for file in all_files if file.endswith('.c')]
-    c_paths = [os.path.join(c_folder, c_file) for c_file in c_files]
 
-    return c_paths
+def mk_ext(name, files):
+    return Extension(name, files + get_liboptv_sources(), include_dirs=get_include_dirs())
 
-c_paths = None
-
-def mk_ext(name, files, add_c_paths = True):
-    global c_paths
-    # Create a Cython extension, adding the C files
-    if add_c_paths:
-        if not c_paths:
-            c_paths = gather_c_sources()
-        files = files + c_paths
-    return Extension(name, files, include_dirs=inc_dirs,
-        pyrex_include_dirs=['.'])
-
-copy_optv_sources()
 
 ext_mods = [
-    mk_ext("optv.tracking_framebuf", ["optv/tracking_framebuf.pyx"]),
-    mk_ext("optv.parameters", ["optv/parameters.pyx"]),
-    mk_ext("optv.calibration", ["optv/calibration.pyx"]),
-    mk_ext("optv.transforms", ["optv/transforms.pyx"]),
-    mk_ext("optv.imgcoord", ["optv/imgcoord.pyx"]),
-    mk_ext("optv.image_processing", ["optv/image_processing.pyx"]),
-    mk_ext("optv.correspondences", ["optv/correspondences.pyx"]),
-    mk_ext("optv.segmentation", ["optv/segmentation.pyx"]),
-    mk_ext("optv.epipolar", ["optv/epipolar.pyx"]),
-    mk_ext("optv.tracker", ["optv/tracker.pyx"]),
-    mk_ext("optv.orientation", ["optv/orientation.pyx"])
+    mk_ext("optv.tracking_framebuf", ["optv/tracking_framebuf.c"]),
+    mk_ext("optv.parameters", ["optv/parameters.c"]),
+    mk_ext("optv.calibration", ["optv/calibration.c"]),
+    mk_ext("optv.transforms", ["optv/transforms.c"]),
+    mk_ext("optv.imgcoord", ["optv/imgcoord.c"]),
+    mk_ext("optv.image_processing", ["optv/image_processing.c"]),
+    mk_ext("optv.correspondences", ["optv/correspondences.c"]),
+    mk_ext("optv.segmentation", ["optv/segmentation.c"]),
+    mk_ext("optv.epipolar", ["optv/epipolar.c"]),
+    mk_ext("optv.tracker", ["optv/tracker.c"]),
+    mk_ext("optv.orientation", ["optv/orientation.c"])
 ]
 
 setup(
     name="optv",
-    cmdclass = {'build_ext': build_ext},
+    cmdclass={
+        'build_ext': BuildExt,
+        'prepare': PrepareCommand,
+    },
     packages=['optv'],
-    ext_modules = ext_mods,
-    package_data = {'optv': ['*.pxd']},
-    data_files = [('sources', ['c_src/src/*']), ('includes', ['c_src/include/*'])],
-    version = '0.1.0',
-    install_requires = [
+    ext_modules=ext_mods,
+    include_package_data=True,
+    data_files=[
+        ('liboptv', glob.glob('liboptv/src/*.c') + glob.glob('liboptv/include/optv/*.h'))
+    ],
+    package_data={
+        'optv': ['*.pxd', '*.c', '*.h'],
+    },
+    version='0.1.0',
+    install_requires=[
         'numpy', 
         'pyyaml',
     ],
-    setup_requires = ['numpy', 'cython']
+    setup_requires=['numpy'],
 )
-
-
