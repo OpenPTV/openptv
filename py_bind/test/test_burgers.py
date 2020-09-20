343aabd2244a055a868960788d964f0783e8a6f9
Alex Liberzon <alex.liberzon@gmail.com>
2020-09-20 19:54:39 +0300

    flake8

--- py_bind/test/test_burgers.py
+++ py_bind/test/test_burgers.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Tests for the Tracker with add_particles using Burgers vortex data with ground truth
+Tests for the Tracker with add_particles using Burgers vortex data
+with ground truth
 
 Created on Mon Apr 24 10:57:01 2017
 
 @author: alexlib
 """
 
-import unittest, yaml, shutil, os
+import unittest
+import yaml
+import os
 from optv.tracker import Tracker
 from optv.calibration import Calibration
 from optv.parameters import ControlParams, VolumeParams, TrackingParams, \
@@ -18,36 +21,39 @@ framebuf_naming = {
     'linkage': b'testing_fodder/burgers/res/ptv_is',
     'prio': b'testing_fodder/burgers/res/whatever'
 }
+
+
 class TestTracker(unittest.TestCase):
     def setUp(self):
         with open(b"testing_fodder/burgers/conf.yaml") as f:
             yaml_conf = yaml.load(f)
         seq_cfg = yaml_conf['sequence']
-        
+
         cals = []
         img_base = []
         print(yaml_conf['cameras'])
         for cix, cam_spec in enumerate(yaml_conf['cameras']):
             cam_spec.setdefault(b'addpar_file', None)
             cal = Calibration()
-            cal.from_file(cam_spec['ori_file'].encode(), cam_spec['addpar_file'].encode())
+            cal.from_file(cam_spec['ori_file'].encode(),
+                          cam_spec['addpar_file'].encode())
             cals.append(cal)
             img_base.append(seq_cfg['targets_template'].format(cam=cix + 1))
-  
+
         cpar = ControlParams(len(yaml_conf['cameras']), **yaml_conf['scene'])
         vpar = VolumeParams(**yaml_conf['correspondences'])
         tpar = TrackingParams(**yaml_conf['tracking'])
         spar = SequenceParams(
             image_base=img_base,
             frame_range=(seq_cfg['first'], seq_cfg['last']))
-        
+
         self.tracker = Tracker(cpar, vpar, tpar, spar, cals, framebuf_naming)
-        
+
     def test_forward(self):
         """Manually running a full forward tracking run."""
         # shutil.copytree(
         #    "testing_fodder/burgers/res_orig/", "testing_fodder/burgers/res/")
-        
+
         self.tracker.restart()
         last_step = 10001
         while self.tracker.step_forward():
@@ -61,29 +67,30 @@ class TestTracker(unittest.TestCase):
                 #     self.failUnless(lines[0] == "2\n")
             last_step += 1
         self.tracker.finalize()
-    
+
     def test_full_forward(self):
         """Automatic full forward tracking run."""
         # shutil.copytree(
         #    "testing_fodder/burgers/res_orig/", "testing_fodder/burgers/res/")
         self.tracker.full_forward()
-        # if it passes without error, we assume it's ok. The actual test is in 
+        # if it passes without error, we assume it's ok. The actual test is in
         # the C code.
-    
+
     def test_full_backward(self):
         """Automatic full backward correction phase."""
-        #shutil.copytree(
+#       shutil.copytree(
         #    "testing_fodder/burgers/res_orig/", "testing_fodder/burgers/res/")
         self.tracker.full_forward()
         self.tracker.full_backward()
-        # if it passes without error, we assume it's ok. The actual test is in 
+        # if it passes without error, we assume it's ok. The actual test is in
         # the C code.
-        
+
     def tearDown(self):
         if os.path.exists("testing_fodder/burgers/res/"):
             # shutil.rmtree("testing_fodder/burgers/res/")
             print("there is a /res folder\n")
             # pass
-        
+
+
 if __name__ == "__main__":
     unittest.main()
